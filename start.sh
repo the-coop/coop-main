7e22459944456a1f263d0dbd935de890c5b8a1d3
L M Fielding <contact@lmfielding.com>
2022-08-12 15:05:09 +0100

    :'(

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+source ~/.bash_profile
 
 # Next thing to try is source profile in here?
 
 
 # Stop all servers and start the server as a daemon
-# cd /coopbot
+cd /coopbot
 
 npm run start
