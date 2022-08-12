0a4c399c40a733b9694923b41b975f4e8c0dc81c
L M Fielding <contact@lmfielding.com>
2022-08-12 14:58:43 +0100

    hmmm

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# Stop all servers and start the server as a daemon
-cd /coopbot
 
-yarn start
+# Next thing to try is source profile in here?
+
+
+# Stop all servers and start the server as a daemon
+# cd /coopbot
+
+npm run start
