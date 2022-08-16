398bab23deb21cca665c760795648891829eb31b
L M Fielding <contact@lmfielding.com>
2022-08-16 22:54:25 +0100

    Clean up

--- operations/minigames/small/chestpop.mjs
+++
@@ -1,9 +0,0 @@
-import { CHANNELS } from "../../../organisation/coop.mjs";
-
-export default class ChestPopMinigame {
-
-    static run() {
-        CHANNELS._postToFeed('ChestPop? 💰');
-    }
-    
-}
